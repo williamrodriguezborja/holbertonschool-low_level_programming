4b6e6c9fc6120b410790db76d291f57d58956028
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-04 23:54:50 -0500

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - last digit
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (char i = 'a'; i < 'z'; i++)
+	{
+		printf("%c", i);
+	}
+	printf("\n");
+	return (0);
+}
