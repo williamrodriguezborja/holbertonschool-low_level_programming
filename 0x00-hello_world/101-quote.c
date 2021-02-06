6547e8a5ecb1512d6d3a69b1f78ae19534f2ed06
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-05 22:19:26 -0500

    diff ok

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,10 @@
 /**
  * main - Print size of variable in this program
  *
- * Return: (0) Success
+ * Return: (1) Error
  */
 int main(void)
 {
-	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(STDERR_FILENO, text, strlen(text));
+	write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" , strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
 	return (1);
 }
