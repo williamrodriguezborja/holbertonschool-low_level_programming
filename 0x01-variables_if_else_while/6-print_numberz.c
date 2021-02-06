6ba5e221f3b5fb8276079bcdd004f54c7d20a692
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-06 14:51:43 -0500

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Print the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57 ; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
