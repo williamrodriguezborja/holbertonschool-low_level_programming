0d9caf9f5c42aa61d559afabbfb5d1042f85b7d2
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-05 22:11:16 -0500

    std err

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@ int main(void)
 {
 	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, text, strlen(text));
+	write(STDERR_FILENO, text, strlen(text));
 	return (1);
 }
