768124993a2e5f5ff41c2e0d94ff77bf94417ef6
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-05 21:59:06 -0500

    write 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <unistd.h>
+/**
+ * main - Print size of variable in this program
+ *
+ * Return: (0) Success
+ */
+int main(void)
+{
+	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, text, strlen(text));
+	return (1);
+}
