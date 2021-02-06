5ab3f40b0919c94d8adc74bb2ce0e488d7412ced
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-05 21:13:00 -0500

    h-5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print Programming is like building a multilingual puzzle
+ *
+ * Return: (0) Success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
