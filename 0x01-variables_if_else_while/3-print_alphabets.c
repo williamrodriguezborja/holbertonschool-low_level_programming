fef88f940b64b615ad40eddeeb8205d774095a1c
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-06 14:30:07 -0500

    alphaBETTY

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print the alphabet a to z and A to Z
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+		putchar(a);
+
+	for (a = 'A'; a<='Z'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
