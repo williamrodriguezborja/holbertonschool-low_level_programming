0a84ff38d95381331b0a5be24d28ca624fc19bbe
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-08 11:37:19 -0500

    10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -5,5 +5,18 @@
  */
 int main(void)
 {
+	int i;
+
+	for (i = 0; i <= 99; i++)
+	{
+		putchar(i / 10 + '0');
+		putchar(i % 10 + '0');
+		if (i < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 	return (0);
 }
