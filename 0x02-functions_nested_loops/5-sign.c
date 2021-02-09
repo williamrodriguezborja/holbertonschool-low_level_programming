325e6349c5c72f8888f3751160dd24be91e27b16
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-09 12:20:27 -0500

    0x02 - 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_sign - print [+,-]
+ * Return: [0, 1]
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar('-');
+	return (-1);
+}
