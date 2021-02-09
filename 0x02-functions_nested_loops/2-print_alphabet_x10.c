e46c6e622e9c5964d98513e53a7c9a9e3c53199c
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-09 11:04:30 -0500

    finish

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,19 @@
 #include "holberton.h"
 /**
- * main - print alphabet by 10
- * Return: Success
+ * print_alphabet_x10 - print alphabet by 10
  */
 void print_alphabet_x10(void)
 {
-	_putchar('h');
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		char c;
+
+		for (c = 'a'; c < 'z'; c++)
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
 }
