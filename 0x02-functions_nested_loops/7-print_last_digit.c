e458a93c7add387256c120bf7766bbfc4626e6c4
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-09 13:11:39 -0500

    0x02 - 7 fix betty style

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,13 @@
 #include "holberton.h"
+/**
+ * print_last_digit - receive a number and print and give last digit
+ * @n: complete number
+ * Return: lastdigit
+ */
 int print_last_digit(int n)
 {
 	if (n < 0)
 		n *= -1;
-	_putchar('0' + (n % 10) );
+	_putchar('0' + (n % 10));
 	return (n % 10);
 }
