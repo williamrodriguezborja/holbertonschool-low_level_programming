29951af723f392b6b11c39b11de7dd910ca3f1ba
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-09 13:57:10 -0500

    0x02 - 8 init

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+
+/**
+ * jack_bauer - print 00:00 to 24:59
+ */
+void jack_bauer(void)
+{
+	int hour;
+	for (hour = 0; hour < 24 ; hour++)
+	{
+
+		int minute;
+
+		for (minute = 0 ; minute < 60; minute++)
+		{
+			_putchar('0' + hour / 10);
+			_putchar('0' + hour % 10);
+			_putchar(':');
+			_putchar('0' + (minute / 10));
+			_putchar('0' + (minute % 10));
+			_putchar('\n');
+		}
+
+	}
+}
