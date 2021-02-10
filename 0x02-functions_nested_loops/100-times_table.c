6564db4c1fa3cca8d6e616f89ae79a7140276449
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-10 12:47:21 -0500

    file without functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,42 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * print_num - int convert to char * and print these
- * @n: number to print
- */
-void print_num(int n)
-{
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
-	int tmp = n;
-	int digits = 0;
-
-	while (tmp)
-	{
-		tmp /= 10;
-		digits++;
-	}
-
-	char numbers[digits];
-	int i = digits - 1;
-
-	tmp = n;
-
-	while (i >= 0)
-	{
-		numbers[i] = (tmp % 10);
-		tmp /= 10;
-		i--;
-	}
-
-	for (i = 0; i < digits  ; i++)
-		_putchar('0' + numbers[i]);
-}
-/**
  * print_times_table - print times tables without > 5 or < to 0
  * @n: number of times tables to print
  */
@@ -62,7 +26,7 @@ void print_times_table(int n)
 					_putchar(' ');
 
 			}
-			print_num(time);
+//			print_num(time);
 		}
 		_putchar('\n');
 	}
