1dafbe4651b9d2f7dc8a9006cd217415a7db493d
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-10 13:12:24 -0500

    only one declarations and for nested

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,7 +6,7 @@
  */
 void print_times_table(int n)
 {
-	int i;
+	int i, j;
 
 	if (n > 15 || n < 0)
 	{
@@ -16,6 +16,9 @@ void print_times_table(int n)
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar('0' + i);
+		for (j = 0; j < n ; j++)
+		{
+			_putchar('0' + j);
+		}
 	}
 }
