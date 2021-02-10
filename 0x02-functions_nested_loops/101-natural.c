bdd673cc016b20cc78a9e0f3add56c106ea9912b
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-10 15:45:02 -0500

    0x2 101 init

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print numbers multiples of 3, 5
+ * Return: Success
+ */
+int main(void)
+{
+	int i, sum;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			sum += i;
+
+	}
+	printf("%d\n", sum);
+	return (0);
+}
