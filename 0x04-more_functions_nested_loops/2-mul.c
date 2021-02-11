d6a05ea0b5b376e8277db5a18f1d0fb661f61256
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-11 18:57:53 -0500

    0x04 - 2 : init

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - perfom a multiplication;
+ * @a: first number
+ * @b: second number
+ * Return: product int
+ */
+int mul(int a, int b)
+{
+	if (a == 0 || b == 0)
+		return 0;
+	return a * b;
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -4,4 +4,5 @@
 void _putchar(char);
 int _isupper(int);
 int _isdigit(int);
+int mul(int, int);
 #endif
