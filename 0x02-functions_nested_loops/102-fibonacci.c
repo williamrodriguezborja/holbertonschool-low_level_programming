9cbe04332ef32f5254e7b6d0268d672e1829e799
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-11 12:52:48 -0500

    0x2 fibonacci betty

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 /**
- * fibonacci - print fibonacci sequence
+ * fibonacci_until - print fibonacci sequence
+ * @until: number limit to print fibonacci
  */
 void fibonacci_until(int until)
 {
-	int f, t1=1, t2=2;
+	int f, t1 = 1, t2 = 2;
+
 	f = t1 + t2;
-	printf("%d , %d, ", t1, t2);
+	printf("%d, %d, ", t1, t2);
 
 	while (f <= until)
 	{
 		if (f != 34)
 			printf("%d, ", f);
 		else
-			printf("%d\n" , f);
+			printf("%d\n", f);
 		t1 = t2;
 		t2 = f;
 		f = t1 + t2;
 	}
 }
+/**
+ * main - print fibonacci sequence until
+ * Return: (Sucess)
+ */
 int main(void)
 {
 	fibonacci_until(50);
