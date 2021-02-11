62b0be50a7cfd153f40c3815abdce36ff1fe8bf2
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-11 13:13:44 -0500

    0x02 - fibonacci change aoutput with comma in last

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,18 +5,19 @@
  */
 void fibonacci_until(int until)
 {
-	int f, t1 = 1, t2 = 2;
+	int f, t1 = 1, t2 = 2 , i = 0;
 
 	f = t1 + t2;
 	printf("%d, %d, ", t1, t2);
 
-	while (f <= until)
+	while (i <= until)
 	{
 		if (f != 34)
 			printf("%d, ", f);
 		t1 = t2;
 		t2 = f;
 		f = t1 + t2;
+		i++;
 	}
 	printf("\n");
 }
