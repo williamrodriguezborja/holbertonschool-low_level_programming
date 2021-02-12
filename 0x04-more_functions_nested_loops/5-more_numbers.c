a805aa340466bfc64785b2c126728cdd0274dca1
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-12 10:03:09 -0500

    0x04 - 5 : fix times to 10

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,12 +4,10 @@
  */
 void more_numbers(void)
 {
-	int i;
+	int i, j;
 
-	for (i = 0 ; i <= 14 ; i++)
+	for (i = 0 ; i <= 10 ; i++)
 	{
-		int j;
-
 		for (j = 0 ; j <= 14; j++)
 		{
 			if (j < 10)
