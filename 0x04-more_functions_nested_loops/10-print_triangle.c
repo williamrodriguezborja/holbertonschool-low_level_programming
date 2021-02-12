fb32ddf93990af961ef6b6c4db54c953fe401232
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-12 13:42:02 -0500

    x04 - 10 init

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * print_triangle - print triangule;
+ * @size: how is big a triangule
+ */
+void print_triangle(int size)
+{
+	if (size < 1)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	int i, j, spaces = size - 1;
+
+	for (i = 0; i < size; i++)
+	{
+		spaces--;
+		for (j = 0; j < size; j++)
+		{
+			if (j <= spaces)
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
