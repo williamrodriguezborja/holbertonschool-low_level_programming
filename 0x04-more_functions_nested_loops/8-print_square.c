c0deeae16313546ef029ea8c7d68e363d19a3e3a
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-12 12:19:15 -0500

    x04 - 8 init

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_square - print a square in base to n columns and rows
+ * @size: n is columns a rows number
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
