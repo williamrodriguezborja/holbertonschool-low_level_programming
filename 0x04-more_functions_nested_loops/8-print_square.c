9be95d867d3893e2ab1c6892da8703c61dea1874
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-12 12:22:58 -0500

    x04 - 8 change to if else

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,18 +6,19 @@
 void print_square(int size)
 {
 	if (size == 0)
-	{
 		_putchar('\n');
-		return;
-	}
-	int i, j;
+	else
+	{
+		int i, j;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
+		for (i = 0; i < size; i++)
 		{
-			_putchar('#');
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
+
 	}
 }
