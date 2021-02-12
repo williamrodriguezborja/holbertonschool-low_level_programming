c3aa462bdde692d516e97f16b72c245cc0d34239
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-12 10:20:25 -0500

    0x04 - 5 : recreate file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * more_numbers - print numbers
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0 ; j <= 14; j++)
+		{
+			if (j < 10)
+				_putchar(j + '0');
+			else
+			{
+				_putchar('0' + (j / 10));
+				_putchar('0' + (j % 10));
+			}
+		}
+		_putchar('\n');
+	}
+}
