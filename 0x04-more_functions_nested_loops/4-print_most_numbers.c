f4127cec8260f2b09324437e082c12633f3be2ef
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-11 20:08:03 -0500

    0x04 - 4 : betty style

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - print numbers
+ */
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		if (i == '2' || i == '4')
+			continue;
+		_putchar(i);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,9 +1,9 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-
 void _putchar(char);
 int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
 void print_numbers(void);
+void print_most_numbers(void);
 #endif
