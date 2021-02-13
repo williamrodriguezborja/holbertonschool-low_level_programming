10c706bfd74e68c7f125f1c2bdf23656b3b77742
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-12 23:30:52 -0500

    0x04 - 5 : 101 - betty style

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -16,6 +16,11 @@ int main(void)
 	print_number(0);
 	_putchar('\n');
 	print_number(-98);
-	_putchar('\n');
+  _putchar('\n');
+  print_number(3);
+  _putchar('\n');
+  print_number(123456789);
+  _putchar('\n');
+
 	return (0);
 }
