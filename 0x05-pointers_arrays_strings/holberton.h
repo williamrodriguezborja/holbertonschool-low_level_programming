58977c08152c58ed252ff75dd4e90f44cc782866
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-16 16:08:05 -0500

    0x5(3)- add holberton.h

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* function that takes a pointer to an int as parameter and updates
+   the value it points to to 98 */
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void _putchar(char);
+
+#endif
