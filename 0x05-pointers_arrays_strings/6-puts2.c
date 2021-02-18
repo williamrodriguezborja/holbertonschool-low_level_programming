d6afa7f20f6cdc8ac5a9c25616f4c747fe64ac45
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-17 19:17:28 -0500

    0x5(6) - betty style

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,14 @@
 #include "holberton.h"
 
 /**
- * puts - print even index
+ * puts2 - print even index
+ * @str: pointer to first char to string array
  */
-void puts2(char *str){
+void puts2(char *str)
+{
 	int i;
-	for (i = 0; str[i]; i+=2)
+
+	for (i = 0; str[i]; i += 2)
 		_putchar(str[i]);
 	_putchar('\n');
 }
