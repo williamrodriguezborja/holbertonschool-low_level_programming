913eb7ede0646ccb1eade524312869b7f19ca801
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-19 11:04:27 -0500

    0x6(2): betty style

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,12 +3,13 @@
  * @dest: destination
  * @src:  source to copy
  * @n:    number of characters to copy
+ * Return: char copied
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	for (;i < n && src[i]; i++)
+	for (; i < n && src[i]; i++)
 		dest[i] = src[i];
 
 	while (i < n)
