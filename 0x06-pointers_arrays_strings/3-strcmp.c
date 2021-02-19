0a0852b9f04dbf1ae4e57b8ad7f2219614d66c3e
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-19 11:47:53 -0500

    0x6(3): betty style

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,5 @@
 /**
- * _strcmp(char *s1, char *s2);
+ * _strcmp - compare to string and return difference in bytes
  * @s1: string 1 to compare with s2
  * @s2: string 2 to compare with s1
  * Return: int 0 or 1
@@ -11,7 +11,7 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] && s2[i])
 	{
 		if (s1[i] - s2[i])
-			return s1[i] - s2[i];
+			return (s1[i] - s2[i]);
 		i++;
 	}
 	return (0);
