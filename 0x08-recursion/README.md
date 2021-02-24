5442c76757e2682d8179f1ac692b0a3be464280f
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-24 14:23:38 -0500

    0x7(8): Init project and setup enviroment holberton.h _putchar and 0-puts_recursion.c

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+# 0x08-recursion
+
+## this is a project to learn Recursion in C because c is Fun.
---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,7 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char);
+void _puts_recursion(char *);
+
+#endif
