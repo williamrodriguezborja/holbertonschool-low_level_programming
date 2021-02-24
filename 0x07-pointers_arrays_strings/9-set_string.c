648612a10dc30257f0192bd5a110906f316eb488
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-23 21:36:30 -0500

    0x7(8): set string init

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+/**
+ * set_string - set one string to other
+ * @s: string to copy
+ * @to: string destiny
+ */
+void set_string(char **s, char *to)
+{
+	int i = 0;
+
+	for (; s[i]; i++)
+	{
+		s[i] = to[i];
+	}
+}
