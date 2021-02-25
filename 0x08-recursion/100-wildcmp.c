23ccce0349a63e70271498692cee0d904e087ae5
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-24 21:32:51 -0500

    0x8(5): use another function recursive separate to _sqrt_recursion

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,8 +1,8 @@
 /**
- * wildcmp -
- * @s1:
- * @s2
- * Return: 
+ * wildcmp - compare to string with recursion
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0 or 1 is are equals
  */
 int wildcmp(char *s1, char *s2)
 {
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,21 +1,28 @@
 /**
 * _sqrt - compute square root of n number
+* @n: number to compute square
+* @i: iterator to search if is divisible by i and result is i and mod is 0
+* Return: square of -1 if not have square exact
 */
 int _sqrt(int n, int i)
 {
 	if (n == i) /* base case not found sqrt */
-		return 0;
-	if (n % i == i)	/* base case success to find sqrt */
+		return (-1);
+	if (n / i == i && n % i == 0)	/* base case success to find sqrt */
+		/* 1024 / 32 == 32 && n % 32 == 0 */
 		return (i);
 	return (_sqrt(n, ++i)); /* recursion */
 }
 /**
  * _sqrt_recursion - compute sqrt of n with recursion
+ * @n: number to compute square root
  * Return: sqrt from n number
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return  _sqrt(n);
+	if (n == 1)
+		return (1);
+	return  (_sqrt(n, 2));
 }
