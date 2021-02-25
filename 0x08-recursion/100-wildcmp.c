93d24eb062ee97229e4ecb9886c13b63242854e2
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-24 23:35:54 -0500

    0x8(100): init wildcard and betty fix

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -6,5 +6,20 @@
  */
 int wildcmp(char *s1, char *s2)
 {
+	/* base case */
+	if (!*s1  &&  !*s2) /* s1 when is == '\0' && s2 when == '\0' */
+		return (1);
 
+	/* if is equal */
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '\0')
+		return (0);
+	if (*s1 == '\0' &&  *s2 != '*')
+		return (0);
+	if (*s1 == '\0' &&  *s2 == '*')
+		return (wilcmp(s1, s2));
+	if (*s2 == "*")
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	return (0);
 }
