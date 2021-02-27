dc382cc4d8d198368d9705b0da521cd763e86b0d
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-27 12:47:08 -0500

    delete c and o files

--- 0x09-static_libraries/100-atoi.c
+++
@@ -1,8 +0,0 @@
-/**
- * _atoi - convert or parse strings to integers
- * @s: string to transform to integer
- * Return: integer in base to s string
- */
-int _atoi(char *s){
-	return (*s);
-}
--- 0x09-static_libraries/9-strcpy.c
+++
@@ -1,9 +0,0 @@
-/**
- * _strcpy(char *dest, char *src)
- * @dest:
- * @src
- */
-char *_strcpy(char *dest, char *src)
-{
-
-}
