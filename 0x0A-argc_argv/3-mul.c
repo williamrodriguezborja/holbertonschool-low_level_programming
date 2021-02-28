2bc8571f5b6eb02d8708c9ad5a649c72a38f8f11
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-27 22:01:55 -0500

    0x0A(3) Restart when only compile

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,20 +7,9 @@
  * @argv: arguments
  * Return: (Success)
  */
-int main(int argc, char **argv)
+int main()
 {
-	/* first edge case Failure*/
-	if (argc < 3)
-	{
-		printf("Error\n");
-		return (EXIT_FAILURE);
-	}
 
-	/* normal case */
-	int num1  = atoi(*(argv + 1));
-	int num2 = atoi(*(argv + 2));
-
-	printf("%d\n", num1 * num2);
 
 	return (EXIT_SUCCESS);
 }
