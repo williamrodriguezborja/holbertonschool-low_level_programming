240464c992288627e6e8db5bc08cfd2407aca7c5
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-02-27 19:17:15 -0500

    0x05(9) Fix betty style

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,14 +1,16 @@
 /**
- * _strcpy(char *dest, char *src)
- * @dest:
- * @src
+ * _strcpy - copy src string to dest
+ * @dest: destination
+ * @src: string
+ * Return: pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	for (; src[i]; i++)
+	for (; src[i] && dest[i]; i++)
 		dest[i] = src[i];
 
+	dest[i] = '\0'
 	return (dest);
 }
