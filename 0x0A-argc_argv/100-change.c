c50c61b0bafdad9de978387a3adb22ffe257a07d
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-01 15:44:27 -0500

    0x0A(100) add cents

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,17 +9,12 @@
  */
 int main(int argc, char *argv[])
 {
-	int money[] = {25, 10, 5, 2, 1};
-	int cents = 0;
-
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	cents = atoi(argv[1]);
-
-	printf("%d %ls\n", cents, money);
+	printf("%d\n", atoi(argv[1]));
 	return (0);
 }
