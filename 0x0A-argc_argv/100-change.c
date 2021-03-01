7195dda548ad01d93f1bdd9075ddbc2b555dbf8c
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-01 15:08:08 -0500

    0x0A(100) Recreate file and add content

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Give number and print min coins used to achive amount
@@ -14,7 +15,7 @@ int main(int argc, char **argv)
 		return (1);
 	}
 
-	printf("%d", arg + 1);
+	printf("%d\n", atoi(*(argv+1)));
 
 	return (0);
 }
