32338c17fce26398e2f573e49e0676347b07e516
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-01 15:26:50 -0500

    0x0A(100) variable amount have error

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,12 +9,14 @@
  */
 int main(int argc, char **argv)
 {
+	int amount = atoi(*(argv + 1));
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	int amount = atoi(*(argv + 1));
+
 	printf("%d\n", amount);
 
 	return (0);
