aab820573c3594abe05481945c282a126d7d2b3d
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-01 14:45:02 -0500

    0x0A(100) Recreate file and add content

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -7,8 +7,29 @@
  * @argv: arguments
  * Return: (Success)
  */
-int main()
+int main(int argc, char **argv)
 {
-	
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (EXIT_FAILURE);
+	}
+
+	int cents[] = {25, 10, 5, 2, 1};
+	int amount = atoi(argv[1]), coin = 0, coins_used = 0, tmp = 0;
+
+	while (tmp < amount)
+	{
+		tmp += cents[coin];
+		coins_used++;
+		if (tmp > amount)
+		{
+			tmp -= cents[coin];
+			coin++;
+			coins_used--;
+		}
+	}
+
+	printf("%d\n", coins_used);
 	return (EXIT_SUCCESS);
 }
