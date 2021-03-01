5992642ef380a0c8fe0937b3fd2078912b8ff39a
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-01 15:52:30 -0500

    0x0A(100) Change all

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,7 +10,7 @@
 int main(int argc, char *argv[])
 {
 	int money[] = {25, 10, 5, 2, 1};
-	int cents = atoi(argv[1]);
+	int cents = atoi(argv[1]), tmp = 0, c = 0, coins_used = 0;
 
 	if (argc != 2)
 	{
@@ -18,6 +18,18 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	printf("%d\n", money[0] + cents);
+	while (tmp < cents)
+	{
+		tmp += money[c];
+		coins_used++;
+		if (tmp > cents)
+		{
+			tmp -= money[c];
+			c++;
+			coins_used--;
+		}
+	}
+
+	printf("%d\n", coins_used);
 	return (0);
 }
