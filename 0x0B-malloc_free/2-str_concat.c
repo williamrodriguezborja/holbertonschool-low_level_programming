daa5935ce46f8c8b321ce8daae0b1035a11853eb
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-04 11:37:28 -0500

    0x0B(2) concatenate string and betty style

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+/**
+* str_concat - concatenate two strings and return new string
+* @s1: first string
+* @s2: second string
+* Return: pointer to char concatenated
+*/
+char *str_concat(char *s1, char *s2)
+{
+	char *str;
+	int i = 0, j = 0, len1 = 0, len2 = 0, size = 0;
+
+	/* edge case any s is null*/
+	if (!s1 || !s2)
+		return ("");
+
+	/* get length of two strings*/
+	for (; s1[len1]; len1++)
+	{}
+
+	for (; s2[len2]; len2++)
+	{}
+
+	size = len1 + len2 + 1;
+
+	/* reserve this space based of lengths*/
+	str = malloc(sizeof(char) * size);
+
+	for (; i < size; i++) /* fill new string */
+	{
+		if (i < len1) /* if copy first string */
+			str[i] = s1[i];
+		else /* else fill second s2 in new string*/
+		{
+			str[i] = s2[j];
+			j++;
+		}
+	}
+
+	str[i] = '\0'; /*add end*/
+	return (str);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,12 +1,15 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-
+/* libs */
 #include <stdio.h>
 #include <stdlib.h>
 
+/* prototypes */
 char *create_array(unsigned int, char);
 char *_strdup(char *);
 int **alloc_grid(int, int);
+char *str_concat(char *, char *);
+
 
 #endif
