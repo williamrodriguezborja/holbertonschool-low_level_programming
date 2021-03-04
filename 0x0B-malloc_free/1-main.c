7937c6a8b4353f5494b42a3bc156c31f3b59c6c9
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-03 19:07:05 -0500

    0x0B(1) Duplicate string with malloc and fix betty style

--- 0x0B-malloc_free/1-main.c
+++ 0x0B-malloc_free/1-main.c
@@ -19,5 +19,13 @@ int main(void)
 	}
 	printf("%s\n", s);
 	free(s);
+	s = _strdup("First, solve the problem. Then, write the code.");
+	if (s == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
 	return (0);
 }
