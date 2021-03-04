584603d213d8cda8d7e61e3f98a4735fff1269d8
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-04 12:34:29 -0500

    0x0B(4) Run valgrind

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * free_grid - returns a pointer to a 2 dimensional array of integers.
+ * @grid: grid pointer to pointer
+ * @height: is the height
+ */
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+	if (height <= 0 || !grid)
+		return;
+
+	for (; i < height; i++)
+		free(*(grid + i));
+
+	free(grid);
+}
