d8e502dc70d153e7c712668ef1c7fa7b49304c12
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-03 21:38:58 -0500

    0x0B(3) add stdio and stdlib headers

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,6 +1,10 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+
+#include <stdio.h>
+#include <stdlib.h>
+
 char *create_array(unsigned int, char);
 char *_strdup(char *);
 int **alloc_grid(int, int);
