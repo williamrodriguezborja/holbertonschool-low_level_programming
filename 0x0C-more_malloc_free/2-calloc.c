96e204b16e493b5070fa1d9a6b073c3c829db945
William Rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-05 12:45:58 -0800

    0x0C(2) - allocate array in memory heap

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* _calloc - Entry point
+* @nmemb: unsigned int
+* @size: unsigned int
+* Return: Always (buffer)
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *buffer;
+
+	int long i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	buffer = malloc((nmemb * size));
+	if (buffer == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i += 1)
+		*(buffer + i) = 0;
+
+	return ((void *)buffer);
+}
