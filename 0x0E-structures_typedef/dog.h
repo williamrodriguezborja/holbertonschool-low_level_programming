2df21a5436aebc1b095942af892ccbc5384b73f2
William Rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-08 15:51:35 -0800

    0x0E(0) - fix betty style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
- * dog - is a structure representation of Dog
+ * struct dog - is a structure representation of Dog
  * @name: Name of dog
  * @age: Age a puppy
  * @owner: Person responsable of this dog
- * Description: Longer description
+ * Description: is a structure representation of Dog
  */
 struct dog
 {
