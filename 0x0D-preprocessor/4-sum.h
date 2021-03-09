1c6afbd926871a66693965975620bc944547abe1
William Rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-08 16:16:17 -0800

    0x0E(2) fix betty and print doggie

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,5 +1,4 @@
 #ifndef SUM_H
 #define SUM_H
-
 #define SUM(num1, num2) (num1 + num2)
 #endif
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,5 @@ struct dog
 };
 
 void init_dog(struct dog *, char *, float, char *);
+void print_dog(struct dog *d);
 #endif
