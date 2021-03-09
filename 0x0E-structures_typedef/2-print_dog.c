9b927f46dcc4e88edd1f7ad05a99bbe796c93c5d
William Rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-08 16:23:15 -0800

    0x0E(2) - attempt fix last checker

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,7 +9,10 @@ void print_dog(struct dog *d)
 	if (!d)
 		return;
 
-	printf("Name: %s\n", d->name ? d->name : "nil");
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner ? d->owner : "nil");
+	if (d->name && d->owner)
+	{
+		printf("Name: %s\n", d->name ? d->name : "nil");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "nil");
+	}
 }
