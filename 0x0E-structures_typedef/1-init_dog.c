d2e9c0c68a48d65401f4d0fbb098628aa61fc9d4
William Rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-08 16:05:32 -0800

    0x0E(1) - fix null case

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 /**
  * init_dog - initialize a dog
  * @d: recive dog struct empty
@@ -8,6 +9,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (!d)
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
