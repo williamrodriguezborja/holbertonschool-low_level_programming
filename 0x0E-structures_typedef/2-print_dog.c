c40950add80aff92eec8974c7b23e086417daa99
William Rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-08 16:37:57 -0800

    0x0E(2) - Delete ternario if

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,10 +9,7 @@ void print_dog(struct dog *d)
 	if (!d)
 		return;
 
-	if (d->name != NULL)
-
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
-
 }
