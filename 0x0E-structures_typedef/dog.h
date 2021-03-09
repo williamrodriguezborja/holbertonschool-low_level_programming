22f5acbb7a55689d0ede2555a486b1f7ec4ea5d3
William Rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-08 17:47:03 -0800

    Add header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,7 +17,7 @@ struct dog
 typedef struct dog dog_t;
 
 void init_dog(struct dog *, char *, float, char *);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
+void print_dog(struct dog *);
+dog_t *new_dog(char *, float , char *);
 
 #endif
