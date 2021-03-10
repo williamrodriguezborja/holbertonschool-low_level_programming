f9d9b4b752a2d18c1e468a9cc78b7ad816881aff
William Rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-10 17:07:41 -0500

    0x0F(2) - Compare and betty style

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,13 +1,10 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-// libraries -------------------------------
-
 #include <stdio.h>
 
-// prototypes --------------------------------
 void print_name(char *, void (*)(char *));
 void array_iterator(int *, size_t, void (*)(int));
-int int_index(int *, int , int (*)(int));
+int int_index(int *, int, int (*)(int));
 
 #endif
