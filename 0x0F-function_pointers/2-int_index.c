e5d84263a6b3a5d7e8b4de7ef1042acc55652f8b
William Rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-10 17:01:00 -0500

    0x0F(2) - Compare and betty style

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,16 +8,15 @@
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0;
-
-	/* corner case */
 	if (!array || !size || !cmp)
 		return (-1);
 
-	for (; i < size; i++)
-	{
+	if (size <= 0)
+		return (-1);
+
+	for (int i = 0; i < size; i++)
 		if (cmp(array[i]))
 			return (i);
-	}
+
 	return (-1);
 }
