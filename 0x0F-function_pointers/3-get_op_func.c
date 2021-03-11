43ff2fe2e6fe23593c122b5e0772a5dc46a74f4a
William Rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-11 08:45:45 -0500

    0x0F(3) - fix operator start with correct and add incorrect char

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,11 +17,13 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}};
 	int i = 0;
 
-	while (ops[i].op)
+	while (ops[i].op && i < 1)
 	{
-		if (*s == ops[i].op[0])
+	if (*s == ops[i].op[0] && !ops[i + 1].op[0])
 			return (ops[i].f);
 		i++;
 	}
+
+
 	return (NULL);
 }
