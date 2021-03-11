90055cd8f9e0b3cd7487db60d542d35fcc3f2827
William Rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-11 18:32:26 -0500

    0x0F(2) - print strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - is a variadic function that print all strings and separator
+ * @separator: character array separator
+ * @n: variatic
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+	char *value;
+	separator = separator ? separator : "";
+
+	va_start(args, n);
+
+	for (; i < n; i++)
+	{
+		value = va_arg(args, char *);
+		printf("%s", value ? value : "(nil)");
+
+		if (i != (n - 1))
+			printf("%s", separator);
+	}
+	va_end(args);
+	printf("\n");
+}
