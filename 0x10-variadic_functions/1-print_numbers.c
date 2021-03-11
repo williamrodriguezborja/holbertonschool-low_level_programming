534c651d273296f2060b9dbfe711f16b71c5118f
William Rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-11 18:09:51 -0500

    0x10(1) - Print with separators and variatics

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+* print_numbers - prints numbers
+* @separator: char separator
+* @n: n variatics numbers;
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i = 0, value;
+
+	va_start(args, n);
+	for (; i < n; i++)
+	{
+		value = va_arg(args, int);
+		printf("%d", value);
+		if (i != n - 1)
+			printf("%s", separator);
+	}
+	va_end(args);
+	printf("\n");
+}
