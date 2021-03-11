29055ac49f8982597c3d1335c83bb291d23718d8
William Rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-11 00:28:28 -0500

    0x0F(3) - only one while

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,10 +18,11 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}};
 	int i = 0;
 
-	while (ops[i].op)
+	while (ops[i].op[0])
 	{
 		if (s[0] == ops[i].op[0])
 			return (ops[i].f);
+		i++;
 	}
 
 	return (NULL);
