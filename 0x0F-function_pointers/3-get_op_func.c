2694061cef4671f3067cde66aad2e6707c96cca6
William Rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-11 00:45:12 -0500

    0x0F(3) - fix 99 error

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,7 +18,7 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}};
 	int i = 0;
 
-	while (ops[i].op[0])
+	while (s && ops[i].op && ops[i].op[0])
 	{
 		if (s[0] == ops[i].op[0])
 			return (ops[i].f);
