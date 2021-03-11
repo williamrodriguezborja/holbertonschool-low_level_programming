c8a9f9e0fdf43af9cd0f8dc05472d146addcd70d
William Rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-11 08:50:21 -0500

    0x0F(3) - fix operator validate if have only one digit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,9 +17,9 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}};
 	int i = 0;
 
-	while (ops[i].op && i < 1)
+	while (ops[i].op)
 	{
-	if (*s == ops[i].op[0] && !ops[i + 1].op[0])
+	if (*s == ops[i].op[0] && !s[i + 1])
 			return (ops[i].f);
 		i++;
 	}
