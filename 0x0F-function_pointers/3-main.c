8aa0e6e863e0f6574531bb5554d7ada13ed5650a
William Rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-11 00:10:57 -0500

    0x0F(3) - refactor main only function

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,7 +8,7 @@
 */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result, i = 0;
+	int num1, num2, result;
 	int (*func)(int, int);
 	char *operator;
 
@@ -20,18 +20,15 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	operator = argv[2];
 
-	for (i = 0; operator[i] && i < 1; i++)
-	{
-		if (operator[i] != '*' && operator[i] != '/' &&
-		operator[i] != '+' && operator[i] != '-' &&	operator[i] != '%')
-		{
-			printf("Error\n");
-			exit(99);
-		}
-	}
-
 	num2 = atoi(argv[3]);
 	func = get_op_func(operator);
+
+	if (!func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
 	result = func(num1, num2);
 	printf("%d\n", result);
 	return (EXIT_SUCCESS);
