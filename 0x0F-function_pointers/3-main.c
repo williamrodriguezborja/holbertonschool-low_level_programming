9224ee55715b2521e2678f203e3a82d4b2a50798
William Rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-10 23:50:00 -0500

    0x0F(3) - refactor main only function

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,51 +1,6 @@
 #include "function_pointers.h"
 
 /**
- * valid_operator - validate operator
- * @c: char to validate
- * Return: 0 or 1 if is valid
-*/
-int valid_operator(char *c)
-{
-	int i = 0;
-
-	if (!c)
-		return (0);
-
-	for (; c[i] && i < 1; i++)
-	{
-		if (c[i] == '*' || c[i] == '/' || c[i] == '+' ||
-		 c[i] ==  '-' || c[i] == '%')
-			return (1);
-	}
-	return (0);
-}
-
-/**
- * to_int - validate if number and return atoi
- * @string: to validate
- * Return: int or 0
- */
-int to_int(char *string)
-{
-	int i;
-
-	for (i = 0; string[i]; i++)
-		if (!(string[i] >= 48 && string[i] <= 57))
-			return (0);
-	return (atoi(string));
-}
-
-/**
- * error - print error and exit with number error
- * @number_of_error: code of exit
-*/
-void error(int number_of_error)
-{
-	printf("Error\n");
-	exit(number_of_error);
-}
-/**
 * main - calculator recibe number 1 operator and number 2
 *        and print result
 * @argc: counter of args
@@ -55,22 +10,40 @@ void error(int number_of_error)
 /* 0x545645646*/
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
+	int num1, num2, result, i = 0;
 	int (*func)(int, int);
 	char *operator;
 
 	if (argc != 4)
-		error(98);
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
 	/* transform to integer */
-	num1 = to_int(argv[1]);
-	operator = argv[2];
-	num2 = to_int(argv[3]);
+	for (i = 1; argv[1][i]; i++)
+		if (!(argv[1][i] >= 48 && argv[1][i] <= 57))
+			return (0);
+	num1 = atoi(argv[1]);
 
+	operator = argv[2];
+
+	for (i = 0; argv[3][i]; i++)
+		if (!(argv[3][i] >= 48 && argv[3][i] <= 57))
+			return (0);
+	num2 = atoi(argv[3]);
 	/* edge case  operator invalid */
 
-	if (!valid_operator(operator))
-		error(99);
+	for (; operator[i] && i < 1; i++)
+	{
+		if (operator[i] != '*' || operator[i] != '/' || operator[i] != '+' ||
+			operator[i] != '-' ||
+			operator[i] != '%')
+		{
+			printf("Error\n");
+			exit(99);
+		}
+	}
 
 	func = get_op_func(operator);
 	result = func(num1, num2);
