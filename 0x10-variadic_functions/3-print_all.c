317793af80dd057abeae60930346bbea3db57baa
William Rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-11 19:43:24 -0500

    0x10(3) - try first attempt print correct output

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,41 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_all - this is a variadic c function that receive format and
+ *             print this elements in stdout
+ * @format: Pointers chars to print elements
+ */
+void print_all(const char *const format, ...)
+{
+	va_list args;
+	int i = 0;
+	/*edge case */
+
+	if (!format)
+		return;
+
+	va_start(args, format);
+
+	for (; format[i]; i++)
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(args, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(args, double));
+			break;
+		case 'i':
+			printf("%d", va_arg(args, int));
+			break;
+		case 's':
+			printf("%s", va_arg(args, char*));
+			break;
+		default:
+			break;
+		}
+	}
+	va_end(args);
+	printf("\n");
+}
