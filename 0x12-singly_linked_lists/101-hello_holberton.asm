c87ec779b6aaef43a8f8e2c041fe4b315b878229
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-19 13:45:59 -0500

    0x12 (101) Assembly print f

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,15 @@
-	global main
-	extern printf
-format:	 .ascii "Hello, Holberton\10"
+	;;  Declare needed C  functions
+	extern	printf	; the C function, to be called
 
-main:
-	push $format
-	call printf
+section .data	; Data section, initialized variables
+fmt:	    db "Hello, Holberton", 10, 0          ; The printf format, "\n",'0'
+	section .text	; Code section.
+	global main	; the standard gcc entry point
+main:			; the program label for the entry point
+	push    rbp	; set up stack frame, must be alligned
+	mov	rdi,fmt
+	call    printf WRT ..plt	; Call C function
+	pop	rbp		; restore stack
 
-	mov rax, 60
-	mov rdi, 0
-	syscall
+	mov	rax,0		; normal, no error, return value
+	ret			; return
