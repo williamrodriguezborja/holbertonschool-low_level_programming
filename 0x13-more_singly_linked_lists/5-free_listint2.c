cb1558fa5a0a16dda921fb6977a51394ed499b6f
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-22 14:00:45 -0500

    0x13 (5) free all pointers

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,17 +2,24 @@
 #include "stdlib.h"
 
 /**
+ * free_listint - free linked list nodes
+ * @head: linked list
+*/
+void free_listint(listint_t *head)
+{
+	if (!head)
+		return;
+	free_listint(head->next);
+	free(head);
+}
+
+
+/**
  * free_listint2 - free linked list nodes
  * @head: linked list pointer
 */
 void free_listint2(listint_t **head)
 {
-	if (!head)
-		return;
-
-	if (!*head)
-		return;
-	free_listint(*head->next);
-	free(*head);
-	free(head);
+	free_listint(*head);
+	*head = NULL;
 }
