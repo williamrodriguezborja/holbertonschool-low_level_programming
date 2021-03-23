a2df205aa52b44d19ef46e0bc8fba1044c924dd8
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-23 17:11:23 -0500

    0x13 (9) Index not valid change validation

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	tmp = *head;
-	for (i = 0 ; i < idx - 1 && tmp->next; i++)
+	for (i = 0 ; i < (idx - 1); i++)
 	{
 		tmp = tmp->next;
 		if (!tmp)
