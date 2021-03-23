d9be40d0dbeb7fab99002d891d9b3c9b5cf73c12
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-23 16:12:18 -0500

    0x13 (9) manage insert with invalid index and free node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	for (i = 0 ; i < idx - 1 && tmp->next; i++)
 		tmp = tmp->next;
 
-	if (i < idx - 1 )
+	if (i < idx - 1)
 	{
 		free(node);
 		return (NULL);
