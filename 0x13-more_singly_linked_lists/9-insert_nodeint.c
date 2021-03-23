682f8d7d1d6b41aecaad95de87a12d4dcbeb8399
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-23 17:16:53 -0500

    0x13 (9) Change fix node procces to add

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,8 +22,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	node->n = n;
 	node->next = NULL;
 
-	if (!*head && !idx) /* first element */
+	if (idx <= 0) /* first element */
 	{
+		node->next = *head;
 		*head = node;
 		return (node);
 	}
