2364ebd0768d723f4bb67ec9c0e145b9f5c1a384
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-25 21:33:30 -0500

    0x14 (0) init manupulation project and first task

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - transform string binary to  unsigned int.
+ * @binary: pointer to strig 
+  * Return: Binary.
+ */
+unsigned int binary_to_uint(const char *binary)
+{
+	unsigned int number, i;
+
+	if (!binary)
+		return (0);
+        
+    for (i = 0, number = 0; binary[i]; i++)
+	{
+		number <<= 1;
+		if (binary[i] != '0' && binary[i] != '1')
+            return (0);
+        number |= (binary[i] - '0');
+	}
+	return (number);
+}
