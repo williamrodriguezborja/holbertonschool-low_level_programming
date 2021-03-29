f487b8f6527f2fdd839805ddabc7b7771dd83081
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-29 15:05:05 -0500

    0x15 (1) check if text_context is null

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,20 +8,19 @@
 int create_file(const char *filename, char *text_content)
 {
 	size_t size;
-	int file_d = 0, write_size = 0;
+	int file_d = 0 ;
 
 	if (!filename)
 		return (-1);
 
-	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file_d == -1)
 		return (file_d);
 
-	for (size = 0; text_content[size]; size++)
+	for (size = 0; text_content && text_content[size]; size++)
 	{}
 
-	write_size = write(file_d, text_content, size);
-	if (write_size == -1)
+	if (write(file_d, text_content, size) == -1)
 		return (-1);
 
 	close(file_d);
