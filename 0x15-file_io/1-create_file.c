145bc0f6948591e6d3ee0e2ee74e311eae44a8f7
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-29 14:58:15 -0500

    0x15 (1) check if write fail

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,21 +8,22 @@
 int create_file(const char *filename, char *text_content)
 {
 	size_t size;
-	int file_d;
+	int file_d = 0, write_size = 0;
 
 	if (!filename)
 		return (-1);
 
-	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
+	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file_d == -1)
 		return (file_d);
 
 	for (size = 0; text_content[size]; size++)
 	{}
-	if (write(file_d, text_content, size) == -1)
+
+	write_size = write(file_d, text_content, size);
+	if (write_size == -1)
 		return (-1);
 
 	close(file_d);
-	return (1); /*read funcition return ssize_t*/
+	return (1); 
 }
