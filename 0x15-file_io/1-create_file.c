f3598aabdefb042a45be29ea547ba854432658e3
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-29 14:42:50 -0500

    0x15 (1) Truncate and create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,11 +13,14 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (0);
 
-	file_d = open(filename, O_CREAT | O_WRONLY, 0600);
+	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	size = 0;
 
 	if (file_d == -1)
 		return (file_d);
+
+	for (;text_content[size] ;size++)
+	{}
 
 	write(file_d, text_content, size);
 	return (1); /*read funcition return ssize_t*/
