35d9a49dbfa4c30083035f1a4c4e31b4933d03dc
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-29 14:47:07 -0500

    0x15 (1) Fix if filename is null return -1 edgecase

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,17 +11,15 @@ int create_file(const char *filename, char *text_content)
 	int file_d;
 
 	if (!filename)
-		return (0);
+		return (-1);
 
 	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	size = 0;
-
+	
 	if (file_d == -1)
 		return (file_d);
 
-	for (;text_content[size] ;size++)
+	for (size = 0;text_content[size] ;size++)
 	{}
-
 	write(file_d, text_content, size);
 	return (1); /*read funcition return ssize_t*/
 }
