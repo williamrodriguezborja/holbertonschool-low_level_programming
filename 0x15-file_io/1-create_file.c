233aed80ed56bad4c9691496f11fa1fdd47548b2
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-29 14:32:03 -0500

    0x15 (1) create with permissions

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * create_file - Create a new file if not exist and fill with content
+ * @filename: name of file
+ * @text_content: Content
+ * Return: size of characters
+ */
+int create_file(const char *filename, char *text_content)
+{
+    size_t size;
+    int file_d;
+
+    if (!filename)
+        return (0);
+
+    file_d = open(filename, O_CREAT | O_WRONLY, 0600);
+    size = 0;
+
+    if (file_d == -1)
+        return (file_d);
+
+    write(file_d, text_content, size);
+    return (1); /*read funcition return ssize_t*/
+}
