a6448a04de7dac4c4715a7a8c11cceeb7231438b
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-29 15:05:49 -0500

    0x15 (1) fix betty style

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	size_t size;
-	int file_d = 0 ;
+	int file_d = 0;
 
 	if (!filename)
 		return (-1);
@@ -22,7 +22,6 @@ int create_file(const char *filename, char *text_content)
 
 	if (write(file_d, text_content, size) == -1)
 		return (-1);
-
 	close(file_d);
-	return (1); 
+	return (1);
 }
