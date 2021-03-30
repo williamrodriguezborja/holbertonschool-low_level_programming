047a27a88c3edbcd51af8320795aec9905f97ec5
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-30 13:22:33 -0500

    0x15 (3) Add while cases

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,68 +1,86 @@
 #include "holberton.h"
-/**
- * read_text - text
- * @buffer: buffer
- * @name: filename
- * @size: size
- */
-void read_text(char *buffer, const char *name, size_t size)
-{
-	int file_d;
+#include <fcntl.h>
+#include <unistd.h>
 
-	file_d = open(name, O_RDONLY);
-	if (file_d == -1 || !name)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", name);
-		exit(98);
-	}
-	read(file_d, buffer, size);
-	close(file_d);
+/**
+ * error - manage errors
+ * @exit_n: buffer
+ * @filename: filename
+ * @fd: file descriptor
+ */
+void error(int exit_n, const char *filename, int fd)
+{
+	if (exit_n == 97)
+		dprintf(STDERR_FILENO, MSG_ERROR_97);
+	else if (exit_n == 98)
+		dprintf(STDERR_FILENO, MSG_ERROR_98, filename);
+	else if (exit_n == 99)
+		dprintf(STDERR_FILENO, MSG_ERROR_99, filename);
+	else
+		dprintf(STDERR_FILENO, MSG_ERROR_100, fd);
+	exit(exit_n);
+}
+/**
+ * read_text - Read text and return characters size
+ * @file_d: file descriptor
+ * @buffer: buffer
+ * @filename: filename
+ * @size: size
+ * Return: size of chars
+ */
+ssize_t read_text(int file_d, char *buffer, const char *filename, size_t size)
+{
+	ssize_t read_size;
+
+	if (file_d == -1)
+		error(98, filename, 0);
+
+	read_size = read(file_d, buffer, size);
+	if (read_size == -1)
+		error(98, filename, 0);
+	return (read_size);
 }
 /**
  * write_text - text
+ * @file_d: file descriptor
  * @buffer: buffer
- * @name: filename
+ * @filename: filename
  * @size: size
  */
-void write_text(char *buffer, const char *name, size_t size)
+void write_text(int file_d, char *buffer, const char *filename, size_t size)
 {
-	int file_d;
-
-	file_d = open(name, O_TRUNC | O_CREAT | O_WRONLY, S_IRUSR |
-	S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (file_d == -1 || !name)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", name);
-		exit(98);
-	}
+	if (file_d == -1 || !filename)
+		error(99, filename, 0);
 	if (write(file_d, buffer, size) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", name);
-		exit(99);
-	}
-	if (close(file_d) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_d);
-		exit(100);
-	}
+		error(99, filename, 0);
 }
 /**
  * main - copy files
  * @argc: counter
- * @argv: argas
+ * @argv: argv
  * Return: success
  */
 int main(int argc, char const *argv[])
 {
 	char buffer[1024];
+	ssize_t read_size;
+	int file_d_read, file_d_write;
+	const char *file_from, *file_to;
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	read_text(buffer, argv[1], 1024);
-	write_text(buffer, argv[2], 1024);
+		error(97, NULL, 0);
+	file_from = argv[1];
+	file_to = argv[2];
+	file_d_read = open(file_from, O_RDONLY);
+	file_d_write = open(file_to, O_TRUNC | O_CREAT | O_WRONLY, 0664);
+	while ((read_size = read_text(file_d_read, buffer, file_from, 1024)) != 0)
+		write_text(file_d_write, buffer, argv[2], 1024);
+
+	if (close(file_d_read) == -1)
+		error(100, NULL, file_d_read);
+
+	if (close(file_d_write) == -1)
+		error(100, NULL, file_d_write);
 	dprintf(STDOUT_FILENO, "%s\n", buffer);
 	return (0);
 }
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -7,9 +7,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+
+/* constant values */
+
+#define MSG_ERROR_97 "Usage: cp file_from file_to\n"
+#define MSG_ERROR_98 "Error: Can't read from file %s\n"
+#define MSG_ERROR_99 "Error: Can't write to %s\n"
+#define MSG_ERROR_100 "Error: Can't close fd %d\n"
+
 /* prototypes*/
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
 
 #endif
