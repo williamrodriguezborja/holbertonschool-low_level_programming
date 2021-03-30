0842b5f1ac923e2f647da45fa3b3feea406e2995
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-29 19:49:15 -0500

    0x15 (3) Copy file fix exit cases

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,52 @@
 #include "holberton.h"
 /**
+ * read_text - text
+ * @buffer: buffer
+ * @name: filename
+ * @size: size
+ */
+void read_text(char *buffer, const char *name, size_t size)
+{
+	int file_d;
+
+	file_d = open(name, O_RDONLY);
+	if (file_d == -1 || !name)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", name);
+		exit(98);
+	}
+	read(file_d, buffer, size);
+	close(file_d);
+}
+/**
+ * write_text - text
+ * @buffer: buffer
+ * @name: filename
+ * @size: size
+ */
+void write_text(char *buffer, const char *name, size_t size)
+{
+	int file_d;
+
+	file_d = open(name, O_TRUNC | O_CREAT | O_WRONLY, S_IRUSR |
+	S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (file_d == -1 || !name)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", name);
+		exit(98);
+	}
+	if (write(file_d, buffer, size) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", name);
+		exit(99);
+	}
+	if (close(file_d) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_d);
+		exit(100);
+	}
+}
+/**
  * main - copy files
  * @argc: counter
  * @argv: argas
@@ -7,8 +54,6 @@
  */
 int main(int argc, char const *argv[])
 {
-	int file_d;
-	const char *from, *dest;
 	char buffer[1024];
 
 	if (argc != 3)
@@ -16,28 +61,8 @@ int main(int argc, char const *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	from = argv[1], dest = argv[2];
-	file_d = open(from, O_RDONLY);
-	if (file_d == -1 || !from)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from);
-		exit(98);
-	}
-	read(file_d, buffer, 1024);
-	file_d = open(dest, O_TRUNC | O_CREAT | O_WRONLY, S_IRUSR |
-	S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	/* read file */
-	if (write(file_d, buffer, 1024) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", from);
-		exit(99);
-	}
-
-	if (close(file_d) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_d);
-		exit(100);
-	}
-	dprintf(STDOUT_FILENO, "%s", buffer);
+	read_text(buffer, argv[1], 1024);
+	write_text(buffer, argv[2], 1024);
+	dprintf(STDOUT_FILENO, "%s\n", buffer);
 	return (0);
 }
