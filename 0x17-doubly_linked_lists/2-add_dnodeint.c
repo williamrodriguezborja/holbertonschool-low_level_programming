b310942b56a9f164e8902ccef58ada2f3512bc18
william <rodriguezborjawilliam@gmail.com>
2021-05-01 23:21:32 -0500

    (0x017)(2) add element in  double linked list with previous node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -31,7 +31,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 
 	node->next = *head;
-  (*head)->prev = node;
+	(*head)->prev = node;
 	*head = node;
 	/* return new node */
 	return (node);
