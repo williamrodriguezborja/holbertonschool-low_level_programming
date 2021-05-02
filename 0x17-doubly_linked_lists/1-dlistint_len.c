9828ae3a0f97e9d5a7c7d02379698c722b64d553
william <rodriguezborjawilliam@gmail.com>
2021-05-01 21:30:05 -0500

    add first with betty style

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -4,7 +4,8 @@
  * @h: linked list
  * Return: length
  */
-size_t dlistint_len(const dlistint_t *h){
+size_t dlistint_len(const dlistint_t *h)
+{
         if (!h)
                 return (0);
         return (1 + dlistint_len(h->next));
