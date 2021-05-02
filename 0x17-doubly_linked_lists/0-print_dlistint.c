b1942db1e01c6263ee252f72b5c560ef9de85c66
william <rodriguezborjawilliam@gmail.com>
2021-05-01 21:20:34 -0500

    0x17 - (0) print with betty style

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,10 +9,6 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	if (!h)
 		return (0);
-	/* case base */
-	if (!h)
-		return (0);
-	/* normal */
 	printf("%d\n", h->n);
 	return (1 + print_dlistint(h->next));
 }
---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * dlistint_len - get len in linked lists
+ * @h: linked list
+ * Return: length
+ */
+size_t dlistint_len(const dlistint_t *h){
+        if (!h)
+                return (0);
+        return (1 + dlistint_len(h->next));
+}
