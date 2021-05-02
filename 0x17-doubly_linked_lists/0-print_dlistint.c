0d291e66402aaa93d9db2c712afa15c847570bf7
william <rodriguezborjawilliam@gmail.com>
2021-05-01 21:04:27 -0500

    0x17 - (0) print with betty style

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,17 +1,18 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - print double linked lists
+ * @h: dlinked list
+ * Return: number of nodes
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t num_nodes = 0;
-
-    if (!h)
-      return (0);
-
-    /* case base */
-    if (!h)
-      return (0);
-
-    /* normal */
-    printf("%d\n", h->n);
-    return (1 + print_dlistint(h->next));
+	if (!h)
+		return (0);
+	/* case base */
+	if (!h)
+		return (0);
+	/* normal */
+	printf("%d\n", h->n);
+	return (1 + print_dlistint(h->next));
 }
