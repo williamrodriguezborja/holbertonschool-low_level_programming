37dd4d66efcedd21d677b0731f78f019a7ff01a3
william <rodriguezborjawilliam@gmail.com>
2021-05-03 18:27:43 -0500

    0x17 (5) refactor without tmp variable and fix betty

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,15 +10,14 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	size_t i;
 
-  /* guard conditon */
+	/* guard conditon */
 	if (!head)
 		return (NULL);
-  /* find index node  */
+	/* find index node  */
 	for (i = 0; i < index && head->next; i++)
-          head = head->next;
-  /* edge case out range */
+		head = head->next;
+	/* edge case out range */
 	if (index > i)
 		return (NULL);
-
 	return (head);
 }
