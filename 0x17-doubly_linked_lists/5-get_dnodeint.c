ce378af3c75d8a3e4042e896733c9e84ee94a17f
william <rodriguezborjawilliam@gmail.com>
2021-05-03 17:03:38 -0500

    0x17 (5) fix betty style

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -21,8 +21,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		tmp = tmp->next;
 	}
 
-  if (index > i)
-          return (NULL);
+	if (index > i)
+		return (NULL);
 
 	return (tmp);
 }
