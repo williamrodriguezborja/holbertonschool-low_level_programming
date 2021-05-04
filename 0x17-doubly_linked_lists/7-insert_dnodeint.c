017360589c698f9b65ffcafbd292107257a2bdcb
william <rodriguezborjawilliam@gmail.com>
2021-05-04 11:38:46 -0500

    0x17 (7) add in the start

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	/* add first node case */
 	if (idx == 0)
 	{
-		node->next = (*h)->next;
+		node->next = (*h);
 		(*h)->prev = node;
 		*h = node;
 		return (node);
