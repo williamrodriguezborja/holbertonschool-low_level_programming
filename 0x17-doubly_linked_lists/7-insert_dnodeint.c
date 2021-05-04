4ca47806a94211689ea7e65ac711e6eb81b0f4b4
william <rodriguezborjawilliam@gmail.com>
2021-05-04 13:22:45 -0500

    0x17 (7) add in the start

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * insert_dnodeint_at_index - insert node in double linked lists
  * @h: linked lists
@@ -11,35 +10,30 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *node, *tmp;
 	size_t i;
-	/* guard condition */
-	if (!h || (int)idx < 0)
+
+	if (!h || (int)idx < 0) /* guard condition */
 		return (NULL);
 	node = malloc(sizeof(dlistint_t));
-
 	if (!node)
 		return (NULL);
 	node->n = n;
-	/*guard conditions*/
-	if (!*h)
+	if (!*h) /*guard conditions*/
 	{
 		*h = node;
 		return (node);
 	}
 	tmp = *h;
-	/* add first node case */
-	if (idx == 0)
+	if (idx == 0)/* add first node case */
 	{
 		node->next = tmp;
 		tmp->prev = node;
 		*h = node;
 		return (node);
 	}
-
 	for (i = 0; tmp->next; i++)
 	{
 		if (i == idx)
-		{
-			node->next = tmp;
+		{	node->next = tmp;
 			tmp->prev->next = node;
 			node->prev = tmp->prev;
 			tmp->prev = node;
@@ -47,12 +41,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		}
 		tmp = tmp->next;
 	}
-
-	if ((i + 1) == idx){
+	if ((i + 1) == idx)
+	{
 		tmp->next = node;
 		node->prev = tmp;
-		return node;
+		return (node);
 	}
-
 	return (NULL);
 }
