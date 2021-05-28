70372d610654dcb1db42dc01a4d39ae82a443708
williamzborja <rodriguezborjawilliam@gmail.com>
2021-05-28 16:51:49 -0500

    0x1A-hash_tables - 0 format betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new = NULL;
 
+	if (size <= 0)
+		return (NULL);
+
 	new = malloc(sizeof(hash_table_t)); /**/
 	if (!new)
 		return (NULL);
