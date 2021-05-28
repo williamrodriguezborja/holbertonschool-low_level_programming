754e21f4dc8bc4714f60813e301feccdbe306418
williamzborja <rodriguezborjawilliam@gmail.com>
2021-05-28 17:00:22 -0500

    0x1A-hash_tables - 0 add edge case size 0 return hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,12 +8,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new = NULL;
 
-	if (size <= 0)
-		return (NULL);
-
 	new = malloc(sizeof(hash_table_t)); /**/
 	if (!new)
 		return (NULL);
+
+	if (size <= 0)
+		return (new);
 
 	new->size = size;
 	new->array = calloc(size, sizeof(hash_node_t));
