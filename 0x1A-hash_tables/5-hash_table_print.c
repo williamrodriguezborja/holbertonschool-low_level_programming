88c515c218227d9d033e1abe98786f149fe8bded
williamzborja <rodriguezborjawilliam@gmail.com>
2021-05-28 22:53:35 -0500

    0x1A-hash_tables - (5) hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - hash table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	size_t i;
+	hash_node_t *node;
+	size_t is_first = 0;
+
+	if (!ht)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		if (node)
+		{
+			if (is_first)
+				printf(", ");
+			printf("%s: %s", node->key, node->value);
+			is_first = 1;
+		}
+	}
+
+	printf("}\n");
+}
