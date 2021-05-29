1dd1ac6455811b793f67cd0dea9900e9cd2f4cb5
williamzborja <rodriguezborjawilliam@gmail.com>
2021-05-28 19:11:04 -0500

    0x1A-hash_tables - (1) fix betty

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -2,21 +2,20 @@
 
 /**
  * hash_djb2 - hash function
- * @str:
- * Return: unsigned long int 
+ * @str: text
+ * Return: unsigned long int hash
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        printf("%d\n", c);
-        // hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-
-    printf("--------------\n");
-    return (hash);
+	hash = 5381;
+	printf("Initial Hash value: \n", hash);
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+		/* printf("%c: %d current hash: %ld \n", c, c, hash); */
+	}
+	return (hash);
 }
