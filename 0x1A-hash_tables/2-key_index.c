da19360450cd7ad2f4c371b29938810c796f91d4
williamzborja <rodriguezborjawilliam@gmail.com>
2021-05-28 19:55:41 -0500

    0x1A-hash_tables - (2) format with betty linter

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,12 +1,19 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - get index
+ * @key: char key
+ * @size: size of hash table
+ * Return: unsigned long int
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int hash;
-    hash = hash_djb2(key);
+	unsigned long int hash;
 
-    if (size == 0)
-        return (0);
-        
-    return (hash % size);
+	hash = hash_djb2(key);
+
+	if (size == 0)
+		return (0);
+
+	return (hash % size);
 }
