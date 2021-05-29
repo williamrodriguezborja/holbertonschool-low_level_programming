dddcce2bd9b0714096cbe28316efda89e13c164f
williamzborja <rodriguezborjawilliam@gmail.com>
2021-05-28 22:55:08 -0500

    0x1A-hash_tables - (5) hash table fix print with quote

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -21,7 +21,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (is_first)
 				printf(", ");
-			printf("%s: %s", node->key, node->value);
+			printf("'%s': '%s'", node->key, node->value);
 			is_first = 1;
 		}
 	}
